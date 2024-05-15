f331eed523ebfff8568eb82a1b869773a8979486
chi-wei-lien <wilsonwilson3124@gmail.com>
2024-05-14 21:59:47 -0400

    rename AddGroupToPostList to AddGroup

--- src/AddGroup/Functions.cs
+++ src/AddGroup/Functions.cs
@@ -13,7 +13,7 @@ using Amazon.Lambda.APIGatewayEvents;
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
 
-namespace AddGroupToPostList;
+namespace AddGroup;
 
 public class Function
 {
